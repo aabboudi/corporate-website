46c0464ca2f070c0f1716776c3133d941d92e3ed
aabboudi <abboudi.abdellah@gmail.com>
2024-08-12 19:32:30 +0100

    Add dynamically generated robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+    const BASE_URL = 'https://xyz.com'
+    return {
+        rules: {
+            userAgent: '*',
+            allow: ['*',],
+            disallow: []
+        },
+        sitemap: `${BASE_URL}/sitemap.xml`
+    }
+}
