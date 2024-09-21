31f256a69e21161e901c49d56542942995a931bd
aabboudi <abboudi.abdellah@gmail.com>
2024-09-22 00:28:51 +0100

    Implemented workflow token to prevent third-party requests

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -7,21 +7,27 @@ import { ReportStatus } from "@/server/report-status";
 export const runtime = 'nodejs';
 
 export async function POST(req: NextRequest) {
-  try {
-    const [newArticle, newOpening] = await Promise.all([
-      GenerateArticle(),
-      GenerateOpening(),
-    ]);
+  const key = req.headers.get("WORKFLOW_TOKEN");
 
-    await Promise.all([
-      DB.insert(Articles).values(newArticle),
-      DB.insert(Openings).values(newOpening),
-    ]);
+  if (req.method === "POST" && key === process.env.WORKFLOW_TOKEN!) {
+    try {
+      const [newArticle, newOpening] = await Promise.all([
+        GenerateArticle(),
+        GenerateOpening(),
+      ]);
 
-    await ReportStatus(200);
-    return NextResponse.json({ message: `${new Date()} - Task completed successfully`});
-  } catch (error) {
-    await ReportStatus(500)
-    return NextResponse.json({ message: `${new Date()} - Task failed: ${error}` }, { status: 500 });
+      await Promise.all([
+        DB.insert(Articles).values(newArticle),
+        DB.insert(Openings).values(newOpening),
+      ]);
+
+      await ReportStatus(200);
+      return NextResponse.json({ message: `${new Date()} - Task completed successfully`});
+    } catch (error) {
+      await ReportStatus(500)
+      return NextResponse.json({ message: `${new Date()} - Task failed: ${error}` }, { status: 500 });
+    }
+  } else {
+    return NextResponse.json({ message: `${new Date()} - Unauthorized request` }, { status: 401 });
   }
 }
